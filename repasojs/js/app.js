6f04de74a6db37696d997f20d8b51dbe97681ed0
Diego Abanto <dabanto21@gmail.com>
2019-01-09 08:16:57 -0500

    scope

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -1,17 +1,47 @@
 // Crear variables
 
-// Variables con var
+// Variables con VAR
 
-var aprendiendo = 'Javascript';
-console.log(aprendiendo);
+/* var aprendiendo = 'Javascript';
+console.log(aprendiendo); */
 
-// Variables const
+// Variables CONST
 
-const aprendiendo02 = 'Javascript';
-console.log(aprendiendo02);
+/* const aprendiendo02 = 'Javascript';
+console.log(aprendiendo02); */
 
-// Variables let
+// Variables LET
 
-let aprendiendo03 = 'Javascript';
+/* let aprendiendo03 = 'Javascript';
 aprendiendo03 = true;
 console.log(aprendiendo03);
+ */
+// Scope
+
+/* var musica = 'Rock';
+
+if(musica){
+    var musica = 'Grunge';
+    console.log('Dentro del IF: ', musica);
+}
+console.log('Fuera del IF: ', musica); */
+
+// Scope con LET
+
+/* let musica = 'Rock';
+
+if(musica){
+    let musica = 'Grunge';
+    console.log('Dentro del IF: ', musica);
+}
+console.log('Fuera del IF: ', musica); */
+
+// Scope con CONST
+
+/* const musica = 'Rock';
+
+if(musica){
+    const musica = 'Grunge';
+    console.log('Dentro del IF: ', musica);
+}
+console.log('Fuera del IF: ', musica); */
