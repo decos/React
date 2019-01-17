6e8eda7800150591c3558f47d9389f86dfa4e744
Diego Abanto <dabanto21@gmail.com>
2019-01-17 14:40:11 -0500

    POO - clases - heredando de una clase

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -443,7 +443,24 @@ class Tarea {
     }
 
     mostrar(){
-        return `${this.nombre} tiene una prioridad de ${this.prioridad}`;
+        //return `${this.nombre} tiene una prioridad de ${this.prioridad}`;
+        console.log(`${this.nombre} tiene una prioridad de ${this.prioridad}`);
+    }
+}
+
+class ComprasPendientes extends Tarea {
+    constructor(nombre, prioridad, cantidad){
+        super(nombre, prioridad);
+        this.cantidad = cantidad;
+    }
+
+    mostrar() {
+        super.mostrar();
+        console.log(`y la cantidad de ${this.cantidad}`);
+    }
+
+    hola() {
+        return "hola";
     }
 }
 
@@ -453,7 +470,13 @@ let tarea2 = new Tarea('Preparar café', 'Media');
 let tarea3 = new Tarea('Pasear al perro', 'Alta');
 let tarea4 = new Tarea('Conocer el mundo', 'Baja');
 
-console.log(tarea1.mostrar());
+/* console.log(tarea1.mostrar());
 console.log(tarea2.mostrar());
 console.log(tarea3.mostrar());
-console.log(tarea4.mostrar());
+console.log(tarea4.mostrar()); */
+
+// Compras
+
+let compra1 = new ComprasPendientes('Jabon', 'Urgente', 3);
+compra1.mostrar();
+console.log(compra1.hola());
